e7185d4a491df64aafe3ba7a21822fbc0051d3c4
DavidQin <david.qin@harman.com>
2024-06-12 11:04:57 +0800

    init

--- Package.swift
+++ Package.swift
@@ -5,19 +5,26 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftSideUI",
+    platforms: [
+        .macOS(.v13), .iOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftSideUI",
-            targets: ["SwiftSideUI"]),
+            targets: ["SwiftSideUI"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftSideUI"),
+            name: "SwiftSideUI"
+        ),
+        
         .testTarget(
             name: "SwiftSideUITests",
-            dependencies: ["SwiftSideUI"]),
+            dependencies: ["SwiftSideUI"]
+        ),
     ]
 )
